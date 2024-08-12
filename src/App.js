9e4550424570323f039d92dc2133d0c68a72226f
Baiquan Fan <baiquan.fan@gmail.com>
2024-08-12 10:13:13 +0300

    add CRUD.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,103 @@
 import './App.css';
 import { Auth } from "./components/auth"
+import { db } from "./config/firebase";
+import { getDocs, collection, doc, addDoc, deleteDoc, updateDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  // new movie states
+  const [newMovieTitle, setNewMovieTitle] = useState("");
+  const [newReleaseDate, setNewReleaseDate] = useState(0);
+  const [isNewMovieOscar, setIsNewMovieOscar] = useState(false);
+
+  // Update title state
+  const [updatedTitle, setUpdatedTitle] = useState("");
+
+  const moviesCollectionRef = collection(db, "movies");
+
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setMovieList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovieList();
+  }, []);
+
+  const onSubmitMovie = async () => {
+    try {
+      await addDoc(moviesCollectionRef, {
+        title: newMovieTitle,
+        releaseDate: newReleaseDate,
+        receivedAnOscar: isNewMovieOscar
+      });
+
+      getMovieList();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const deleteMovie = async (id) => {
+    const movieDoc = doc(db, "movies", id);
+    await deleteDoc(movieDoc);
+
+    getMovieList();
+  };
+
+  const updateMovieTitle = async (id) => {
+    const movieDoc = doc(db, "movies", id);
+    await updateDoc(movieDoc, { title: updatedTitle});
+
+    getMovieList();
+  };
+
   return (
     <div className="App">
       <Auth />
+      <div>
+        <input
+          placeholder="Movie title..."
+          onChange={(e) => setNewMovieTitle(e.target.value)}
+        />
+        <input
+          placeholder="Release Date..."
+          type="number" onChange={(e) => setNewReleaseDate(Number(e.target.value))}
+        />
+        <input
+          type="checkbox"
+          checked={isNewMovieOscar}
+          onChange={(e) => setIsNewMovieOscar(e.target.checked)}
+        />
+        <label>Received an Oscar</label>
+        <button onClick={onSubmitMovie}>Submit Movie</button>
+      </div>
+      <div>
+        {movieList.map((movie) => (
+          <div>
+            <h1 style={{ color: movie.receivedAnOscar ? "green" : "red" }}>
+              {movie.title}
+            </h1>
+            <p>Date: {movie.releaseDate}</p>
+            <button onClick={() => deleteMovie(movie.id)}>Delete Movie</button>
+            <input
+              placeholder="new title..."
+              onChange={(e) => setUpdatedTitle(e.target.value)}
+            />
+            <button onClick={() => updateMovieTitle(movie.id)}>Update Title</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBi8LLkL4c4qU-YUXZnnXiR4uvuhl5uiHw",
@@ -16,3 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleAuthProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
